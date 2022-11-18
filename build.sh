1e33c33d1e1453c619605c06799bf07f2354cc3c
clm2755 <33093771+clm2755@users.noreply.github.com>
2022-11-18 12:58:36 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,15 +6,12 @@ rm -f *.class
 find . -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -d build -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar src/main/java/*.java
-if [ $? -ne 0 ] ; then echo SOURCE BUILD FAILED!; exit 1; fi
-
-javac -d build -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:src/main/java/ src/test/java/*.java
-if [ $? -ne 0 ] ; then echo TEST BUILD FAILED!; exit 1; fi
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
-java -cp build:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java -cp build RunEdgeConvert
+java RunEdgeConvert
